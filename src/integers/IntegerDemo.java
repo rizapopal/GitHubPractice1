febaae4fe6dc43f1c3e3769bc8a575916d0d1a3d
rizapopal <rizapopal@macbook-pro>
2021-12-23 00:58:11 -0500

    Updated PracticeOneDemo and Added IntegerDemo

---
+++ src/integers/IntegerDemo.java
@@ -0,0 +1,15 @@
+package integers;
+
+public class IntegerDemo {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 50 * ++a + 3;
+		
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
--- src/practice1/PracticeOneDemo.java
+++ src/practice1/PracticeOneDemo.java
@@ -3,8 +3,9 @@ package practice1;
 public class PracticeOneDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Practice 1...");
+		System.out.println("Updated Code");
 	}
 
 }
